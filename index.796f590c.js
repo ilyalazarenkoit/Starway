e19d71bdd480ae4057939bff8ec313d6e494e723
ilyalazarenkoit <112101017+ilyalazarenkoit@users.noreply.github.com>
2022-11-14 16:16:41 +0000

    Deploying to gh-pages from @ ilyalazarenkoit/Starway@4a0f6f5251211387f0f6670ebaa51cbdd11923e9 🚀

---
+++ index.796f590c.js
@@ -0,0 +1,2 @@
+document.querySelector(".film__list");window.addEventListener("load",(function(){})),fetch('https://api.themoviedb.org/3/search/movie?api_key=d7175633e0b5107da3a11b631113cb80&query="john"&rating=6').then((e=>e.json())).then((e=>console.log(e))),console.log("hello world");
+//# sourceMappingURL=index.796f590c.js.map
