1c7400f9b911bef992f32f06e83dcbf0f68fa446
Lukast2008 <lukast2008@gmail.com>
2022-11-15 10:55:38 +0200

    add storage

--- src/index.js
+++ src/index.js
@@ -5,5 +5,3 @@
 // } from './js-modules/main-page-default';
 
 import { getData } from './js-modules/localstorage';
-
-getData();
--- src/js-modules/localstorage.js
+++ src/js-modules/localstorage.js
@@ -1,37 +1,67 @@
-// const axios = require('axios');
+const axios = require('axios');
 
 const BASE_URl = 'https:api.themoviedb.org';
+const API_KEY = 'd7175633e0b5107da3a11b631113cb80';
 
-const key = 'd7175633e0b5107da3a11b631113cb80';
+const markupPlace = document.querySelector('body');
 
-const createMarkup = document.querySelector('body');
-let addFilm = null;
-
-export async function getData() {
-  const films = await fetch(`${BASE_URl}/3/movie/76341?api_key=${key}`).then(
-    response => response.json()
-  );
-
-  return films;
+function createMarkup() {
+  return `<form>
+            <button  class="library-watched" type="button">Watched</button>
+            <button class="library-queue" type="button" >queue</button>
+          </form>`;
 }
 
-const data = getData().then(data => {
-  addFilm = data.title;
-});
-console.log(addFilm);
-function markup() {
-  return `<div class="films">
-  <form><button>1</button>
-  <button>2</button>
-  </form>
-  
-  <ul>
-        <li> <p class="info-item">
-                        <b>Likes</b>${addFilm}
-                      </p></li></ul>
-</div>`;
+markupPlace.innerHTML = createMarkup();
+
+const handleButtonClickWatched = document.querySelector('.library-watched');
+
+const handleButtonClickQueue = document.querySelector('.library-queue');
+
+handleButtonClickWatched.addEventListener('click', openWatchedLibrary);
+
+handleButtonClickQueue.addEventListener('click', openQueueLibrary);
+
+// export async function getData() {
+//   const films = await fetch(
+//     `${BASE_URl}/3/trending/movie/day?api_key=${API_KEY}`
+//   ).then(response => response.json());
+//   return films;
+// }
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+// localStorage.setItem('ui-theme', 'light');
+// localStorage.setItem('sidebar', 'expanded');
+// localStorage.setItem('notification-level', 'mute');
+
+const queue = {
+  theme: 'dark',
+  isAuthenticated: true,
+  options: [1, 2, 3],
+};
+
+const watched = {
+  theme: 'white',
+  isAuthenticated: false,
+  options: [4, 5, 6],
+};
+
+localStorage.setItem('queue', JSON.stringify(queue));
+
+localStorage.setItem('watched', JSON.stringify(watched));
+
+// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function openWatchedLibrary() {
+  console.log('Watched');
+  console.log('queue');
+  const dataQ = localStorage.getItem('queue');
+
+  console.log(dataQ);
 }
 
-const roz = markup(addFilm);
+function openQueueLibrary() {
+  console.log('queue');
+  const dataQ = localStorage.getItem('queue');
 
-createMarkup.innerHTML = roz;
+  console.log(dataQ);
+}
