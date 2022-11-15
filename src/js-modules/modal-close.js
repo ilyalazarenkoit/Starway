f94e8f596d1f76c9568487a94af3f0033a7a8253
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2022-11-15 21:10:28 +0200

    2

--- src/js-modules/modal-close.js
+++ src/js-modules/modal-close.js
@@ -2,12 +2,12 @@ const overlay = document.querySelector('.modal__backdrop');
 const modalCardMovie = document.querySelector('.modal_movie_card');
 const modalClose = document.querySelector('.modal__close-btn');
 
-const pickFilm = document.querySelector('.film__img');
-
+const pickFilm = document.querySelector('.film__link');
+console.log(pickFilm);
 pickFilm.addEventListener('click', onModalOpenBtn);
 
 function onModalOpenBtn() {
-  overlay.classList.remove('is-hidden');
+  overlay.classList.toggle('is-hidden');
 }
 
 modalClose.addEventListener('click', onModalCloseBtn);
