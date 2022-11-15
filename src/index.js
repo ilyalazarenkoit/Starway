07d7012313e9fe2250ca883f631a10075e046c9e
Lukast2008 <lukast2008@gmail.com>
2022-11-15 16:48:46 +0200

    allmoust done locale storege

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import { getData } from './js-modules/localstorage';
-
 import {
   film_list,
   onLoad,
   renderDefaultMarkup,
 } from './js-modules/main-page-default';
 import './js-modules/render-header';
+import { getData } from './js-modules/localstorage';
--- src/js-modules/localstorage.js
+++ src/js-modules/localstorage.js
@@ -1,75 +1,102 @@
 const axios = require('axios');
 
-const BASE_URl = 'https://api.themoviedb.org/3/trending/all/day?api_key=';
+const arrWatched = [];
+const arrQueue = [];
+
+// const BASE_URl = 'https://api.themoviedb.org/3/trending/all/day?api_key=';
+const BASE_URl = 'https://api.themoviedb.org/3/movie/';
 const API_KEY = 'd7175633e0b5107da3a11b631113cb80';
+
+// https://api.themoviedb.org/3/movie/ 505642?api_key=d7175633e0b5107da3a11b631113cb80&language=en-US
 
 const markupPlace = document.querySelector('body');
 
-function getItem() {
-  return fetch(`${BASE_URl}${API_KEY}`);
+function getItem(id) {
+  return fetch(`${BASE_URl}${id}?language=en-US&api_key=${API_KEY}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
 }
 
-getItem()
-  .then(response => response.json())
-  .then(response => console.log(response.results));
+// function createMarkup() {
+//   return `<form>
+//             <button  class="add-library-watched"  data-id="505642" type="button">AddWatched</button>
+//             <button class="add-library-queue"  data-id="505642" type="button" >AddQueue</button>
 
-function createMarkup() {
-  return `<form>
-            <button  class="library-watched" type="button">Watched</button>
-            <button class="library-queue" type="button" >queue</button>
-          </form>`;
-}
+//             <button  class="library-watched" type="button">Watched</button>
+//             <button class="library-queue" type="button" >queue</button>
+//           </form>`;
+// }
+// markupPlace.innerHTML = createMarkup();
 
-markupPlace.innerHTML = createMarkup();
-
+const handleButtonClickAddWatched = document.querySelector(
+  '.add-library-watched'
+);
+const handleButtonClickAddQueue = document.querySelector('.add-library-queue');
 const handleButtonClickWatched = document.querySelector('.library-watched');
-
 const handleButtonClickQueue = document.querySelector('.library-queue');
 
+// handleButtonClickAddWatched.addEventListener('click', event => {
+//   const id = event.target.dataset.id;
+//   addWatchedLibrary(id);
+// });
+// handleButtonClickAddQueue.addEventListener('click', event => {
+//   const id = event.target.dataset.id;
+//   addQueueLibrary(id);
+// });
+
 handleButtonClickWatched.addEventListener('click', openWatchedLibrary);
-
 handleButtonClickQueue.addEventListener('click', openQueueLibrary);
-
-// export async function getData() {
-//   const films = await fetch(
-//     `${BASE_URl}/3/trending/movie/day?api_key=${API_KEY}`
-//   ).then(response => response.json());
-//   return films;
-// }
-// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-// localStorage.setItem('ui-theme', 'light');
-// localStorage.setItem('sidebar', 'expanded');
-// localStorage.setItem('notification-level', 'mute');
-
-const queue = {
-  theme: 'dark',
-  isAuthenticated: true,
-  options: [1, 2, 3],
-};
-
-const watched = {
-  theme: 'white',
-  isAuthenticated: false,
-  options: [4, 5, 6],
-};
-
-localStorage.setItem('queue', JSON.stringify(queue));
-
-localStorage.setItem('watched', JSON.stringify(watched));
 
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function openWatchedLibrary() {
-  console.log('Watched');
-  console.log('queue');
-  const dataQ = localStorage.getItem('queue');
+function openLibrary(kay) {
+  const dataW = localStorage.getItem(kay);
+  return dataW ? JSON.parse(dataW) : [];
+}
 
-  console.log(dataQ);
+function openWatchedLibrary() {
+  const dataW = localStorage.getItem('arrWatched');
+  console.log(dataW);
 }
 
 function openQueueLibrary() {
-  console.log('queue');
-  const dataQ = localStorage.getItem('queue');
-
+  const dataQ = localStorage.getItem('arrQueue');
   console.log(dataQ);
 }
+
+function addWatchedLibrary(id) {
+  const arrWatched = openLibrary('arrWatched');
+  if (arrWatched.includes(id)) {
+    return;
+  }
+  arrWatched.push(id);
+  localStorage.setItem('arrWatched', JSON.stringify(arrWatched));
+}
+
+function addQueueLibrary(id) {
+  const arrQueue = openLibrary('arrQueue');
+  if (arrQueue.includes(id)) {
+    return;
+  }
+  arrQueue.push(id);
+  localStorage.setItem('arrQueue', JSON.stringify(arrQueue));
+}
+
+function getDataFilms() {
+  const arrWatched = openLibrary('arrWatched');
+  const promises = [];
+  arrWatched.forEach(id => {
+    const promise = getItem(id);
+    promises.push(promise);
+  });
+  Promise.all(promises)
+    .then(response => console.log(response))
+    .catch(err => console.log(error));
+}
+
+getDataFilms();
