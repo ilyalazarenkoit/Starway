51a7b9e4d49ff4b7edcd2efa7ac4818ca6cfe461
Albina Shmatko <albina.shmatko@gmail.com>
2022-11-16 14:39:39 +0200

    Updates the search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-
-import "./js-modules/main-page-default"
-import "./js-modules/genres"
+import './js-modules/main-page-default';
+import './js-modules/genres';
 import './js-modules/render-header';
-import './js-modules/loader'
-
+import './js-modules/loader';
+import './js-modules/api';
+import './js-modules/search_film';
---
+++ src/js-modules/search_film.js
@@ -0,0 +1,34 @@
+import Films__API from './api';
+import Notiflix from 'notiflix';
+import { renderMarkup } from './main-page-default';
+const filmsApi = new Films__API();
+
+const searchEl = document.querySelector('.header__form');
+
+searchEl.addEventListener('submit', onInputSearch);
+
+export function onInputSearch(event) {
+  event.preventDefault();
+  const inputValue = event.currentTarget.elements.search.value
+    .trim()
+    .toLowerCase();
+  if (inputValue === '') {
+    Notiflix.Notify.info('Please, type movie name');
+    return;
+  }
+  console.log(inputValue);
+  filmsApi.setQuery(inputValue);
+  filmsApi
+    .getFilmsByQuery()
+    .then(({ results }) => {
+      if (results.length === 0) {
+        Notiflix.Notify.failure('No such movie');
+        return;
+      }
+      return renderMarkup(results);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  event.currentTarget.reset();
+}
