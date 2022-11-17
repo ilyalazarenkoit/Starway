b6992ab01cac6e2778d3bf5cb03b7961bd25dd64
ilyalazarenkoit <ilyalazarenkoit@gmail.com>
2022-11-17 14:03:21 +0200

    commit blabla

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './js-modules/render-header';
 import './js-modules/loader';
 import './js-modules/api';
 import './js-modules/search_film';
-// import './js-modules/login-modal';
-// import './js-modules/authentication-login';
-// import './js-modules/authentication-sign';
-// import './js-modules/is-auth';
+import './js-modules/login-modal';
+import './js-modules/authentication-login';
+import './js-modules/authentication-sign';
+import './js-modules/is-auth';
 
