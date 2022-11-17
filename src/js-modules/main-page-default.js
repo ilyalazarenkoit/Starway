eea2d81edd6a6ba28f90d840b64db0c3a0e55613
Lukast2008 <lukast2008@gmail.com>
2022-11-17 16:28:42 +0200

    fix problem

--- src/js-modules/main-page-default.js
+++ src/js-modules/main-page-default.js
@@ -3,6 +3,9 @@ import { genres } from './genres';
 let processed;
 let film_list = document.querySelector('.film__list');
 let markup = '';
+export let page = 1;
+export let insertPage = `&page=${page}`;
+let totalPages;
 const TRENDING_URL = 'https://api.themoviedb.org/3/trending/movie/day?api_key=';
 const API_KEY = 'd7175633e0b5107da3a11b631113cb80';
 const LANGUAGE = '&language=en-US';
@@ -18,20 +21,25 @@ function getGenreByID(array, ids = []) {
   return str;
 }
 
-function renderMarkup(response) {
-  markup = response.results
+export function renderMarkup(results) {
+  console.log(results);
+  markup = results
     .map(item => {
+      console.log(item);
       return `<li class="film__card" data-id="${item.id}">
             <img class="film__img" src="https://image.tmdb.org/t/p/w500/${
               item.poster_path
             }" alt=${item.title}>
+            <div class="film__wrapper">
             <h2 class="film__name">${item.title}</h2>
             <p class="film__genre">${getGenreByID(genres, item.genre_ids)} | ${(
         item.release_date ||
         item.first_air_date ||
         ''
       ).slice(0, 4)}</p>
-            </li>`;
+      <p class="film__rate">${item.vote_average.toFixed(1)}</p>
+      </div>
+      </li>`;
     })
     .join('');
   film_list.innerHTML = markup;
