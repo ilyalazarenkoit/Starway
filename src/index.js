36ae0efa461a384b8c12bce23d569a3e585eacbe
alex-main-academy <oleksandrilnytskyi@gmail.com>
2022-11-18 12:00:26 +0100

    switch-theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './js-modules/main-page-default';
 import './js-modules/genres';
 import './js-modules/render-header';
-import './js-modules/loader'
-import './js-modules/modal-footer'
+import './js-modules/loader';
+import './js-modules/modal-footer';
 import './js-modules/login-modal';
 import './js-modules/authentication-login';
 import './js-modules/authentication-sign';
 import './js-modules/is-auth';
+import './js-modules/switch-theme';
---
+++ src/js-modules/switch-theme.js
@@ -0,0 +1,40 @@
+const toggleSwitch = document.querySelector('.toggle-button');
+
+const darkThemeSettings = {
+  backgroundColor: 'black',
+  color: 'var(--secondary-color-of-text)',
+  checked: 'true',
+};
+const ligthThemeSettings = {
+  backgroundColor: 'white',
+  color: 'var(--primary-color-of-text)',
+  checked: 'false',
+};
+
+function changeBodyStyles() {
+  document.body.style.cssText = `
+        background-color: ${
+          JSON.parse(localStorage.getItem('theme')).backgroundColor
+        };
+        color: ${JSON.parse(localStorage.getItem('theme')).color};
+    `;
+
+  if (JSON.parse(localStorage.getItem('theme')).checked === 'true') {
+    toggleSwitch.setAttribute('checked', true);
+  } else {
+    toggleSwitch.removeAttribute('checked');
+  }
+}
+changeBodyStyles();
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    localStorage.setItem('theme', JSON.stringify(darkThemeSettings));
+    changeBodyStyles();
+  } else {
+    localStorage.setItem('theme', JSON.stringify(ligthThemeSettings));
+    changeBodyStyles();
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
