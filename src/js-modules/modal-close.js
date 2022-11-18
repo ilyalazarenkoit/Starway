ce6a3583c41e27f5c5cb290ed94b6df801df2fb4
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2022-11-18 12:14:18 +0200

    1`

---
+++ src/js-modules/modal-close.js
@@ -0,0 +1,42 @@
+import './main-page-default';
+import { fetchFilmPick, createMarkupMovieInfo } from './modal-markup';
+
+const overlay = document.querySelector('.modal__backdrop');
+const modalCardMovie = document.querySelector('.modal_movie_card');
+const modalClose = document.querySelector('.modal__close-btn');
+
+const pickFilm = document.querySelector('.film__list');
+pickFilm.addEventListener('click', onModalOpenBtn);
+
+pickFilm.addEventListener('click', async event => {
+  const id = event.target.closest('.film__card').dataset.id;
+  const response = await fetchFilmPick(id);
+  createMarkupMovieInfo(response);
+});
+
+function onModalOpenBtn() {
+  overlay.classList.toggle('is-hidden');
+}
+
+modalClose.addEventListener('click', onModalCloseBtn);
+overlay.addEventListener('click', onBackdropClick);
+window.addEventListener('Escape', onPushEsc);
+
+function onModalCloseBtn() {
+  modalCardMovie.innerHTML = '';
+  overlay.classList.add('is-hidden');
+}
+
+function onBackdropClick(event) {
+  if (event.target === event.currentTarget) {
+    onModalCloseBtn();
+  }
+}
+
+function onPushEsc(event) {
+  if (event.code === 'Escape') {
+    onModalCloseBtn();
+  }
+}
+
+export { onBackdropClick, onPushEsc, onModalCloseBtn };
