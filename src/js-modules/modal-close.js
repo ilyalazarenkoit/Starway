0ceca5180e148586e14243fb9749224aca3dfddd
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2022-11-19 13:53:18 +0200

    19.11

--- src/js-modules/modal-close.js
+++ src/js-modules/modal-close.js
@@ -4,27 +4,29 @@ import { fetchFilmPick, createMarkupMovieInfo } from './modal-markup';
 const overlay = document.querySelector('.modal__backdrop');
 const modalCardMovie = document.querySelector('.modal_movie_card');
 const modalClose = document.querySelector('.modal__close-btn');
-
 const pickFilm = document.querySelector('.film__list');
+
 pickFilm.addEventListener('click', onModalOpenBtn);
-console.log(pickFilm);
+modalClose.addEventListener('click', onModalCloseBtn);
+overlay.addEventListener('click', onBackdropClick);
+
+function onModalOpenBtn() {
+  overlay.classList.toggle('is-hidden');
+  window.addEventListener('keydown', onPushEsc);
+  document.body.classList.add('modal-block');
+}
+
 pickFilm.addEventListener('click', async event => {
   const id = event.target.closest('.film__card').dataset.id;
   const response = await fetchFilmPick(id);
   createMarkupMovieInfo(response);
 });
 
-function onModalOpenBtn() {
-  overlay.classList.toggle('is-hidden');
-}
-
-modalClose.addEventListener('click', onModalCloseBtn);
-overlay.addEventListener('click', onBackdropClick);
-window.addEventListener('Escape', onPushEsc);
-
 function onModalCloseBtn() {
   modalCardMovie.innerHTML = '';
   overlay.classList.add('is-hidden');
+  window.removeEventListener('keydown', onPushEsc);
+  document.body.classList.remove('modal-block');
 }
 
 function onBackdropClick(event) {
